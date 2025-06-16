60973e929b293e0a2baac623a900ddeca189a199
FR1029 <24b1072@iitb.ac.in>
2025-06-16 18:43:59 +0000

    weak 3 added

---
+++ Week3/next_greater_element_II.py
@@ -0,0 +1,13 @@
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        result = [-1] * n
+        stack = []
+        for i in range(2 * n - 1, -1, -1):
+            index = i % n
+            while stack and nums[stack[-1]] <= nums[index]:
+                stack.pop()
+            if stack:
+                result[index] = nums[stack[-1]]
+            stack.append(index)    
+        return result
